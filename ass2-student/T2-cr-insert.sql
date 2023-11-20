e239a4cf1c6089c06fdc438af768dee9f2b31bed
Christian Morabito <cmor0056@student.monash.edu>
2023-11-21 09:49:45 +1100

    Completed the first task (relational algebra). Extracted assignment SQL files into ass2-student folder.

---
+++ ass2-student/T2-cr-insert.sql
@@ -0,0 +1,46 @@
+--*****PLEASE ENTER YOUR DETAILS BELOW*****
+--T2-cr-insert.sql
+
+-- ITO Assignment 2 Task 2
+
+--Student ID:
+--Student Name:
+
+/* Comments for your marker:
+
+
+
+
+*/
+
+
+-- ENSURE that your SQL code is formatted and has a semicolon (;)
+-- at the end of every statement. When marked this will be run as
+-- a script.
+
+
+-- Insert into ENTRY
+
+
+
+
+
+
+
+
+
+
+
+-- Insert into TEAM
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ ass2-student/T6a-cr-json.sql
@@ -0,0 +1,26 @@
+--*****PLEASE ENTER YOUR DETAILS BELOW*****
+--T6a-cr-json.sql
+
+-- ITO Assignment 2 Task 6a
+
+--Student ID:
+--Student Name:
+
+/* Comments for your marker:
+
+
+
+
+*/
+
+-- ENSURE that your SQL code is formatted and has a semicolon (;)
+-- at the end of every statement. When marked this will be run as
+-- a script.
+
+/*
+(a)
+*/
+
+
+
+
