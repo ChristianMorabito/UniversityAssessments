868fb276e8c29ce2439cd20d34ed3bd37396e2eb
Christian Morabito <cmor0056@student.monash.edu>
2023-11-27 21:05:03 +1100

    Still working on Task3 C, as I am stuck. But completed D in the meantime.

--- Assignments/Ass2/T4-cr-queries.sql
+++ Assignments/Ass2/T4-cr-queries.sql
@@ -65,22 +65,54 @@ ORDER BY
 /*
 (c)
 */
-
+select * from entry where comp_no = 1;
 
 SELECT
+    c.comp_no AS COMPNO,
+    c.comp_fname AS COMPFNAME,
     c.comp_lname AS COMPLNAME,
     c.comp_gender AS COMPGENDER,
-    count(ent.carn_date) as asfd
+    count(EXTRACT(year FROM ent.carn_date)),
+    count(EXTRACT(year FROM ent.carn_date))
+    
+    
+
 FROM
     entry ent
     JOIN competitor c ON c.comp_no = ent.comp_no 
     JOIN carnival car ON car.carn_date = ent.carn_date
+WHERE
+    EXTRACT(YEAR FROM SYSDATE) BETWEEN EXTRACT(YEAR FROM ent.carn_date) - 2
+    AND EXTRACT(YEAR FROM SYSDATE)
 GROUP BY
-    c.comp_no, c.comp_lname, c.comp_gender
+    c.comp_no,
+    c.comp_fname,
+    c.comp_lname,
+    c.comp_gender;
+    
+    
+    
 
 /*
 (d) 
 */
+
+SELECT
+    TO_CHAR(c.carn_date, 'DD-Mon-YYYY') AS CARNIVAL_DATE,
+    c.carn_name,
+    COUNT(e.entry_id) AS TOTAL_ENTRIES5KM
+FROM
+    carnival c
+    JOIN entry e ON c.carn_date = e.carn_date
+WHERE
+    c.carn_date BETWEEN TO_DATE('01-Jan-2023', 'DD-Mon-YYYY') 
+    AND TO_DATE('31-Dec-2023', 'DD-Mon-YYYY')
+    AND e.eventtype_code = '5K'
+GROUP BY
+    c.carn_date, c.carn_name
+ORDER BY
+    TOTAL_ENTRIES5KM DESC,
+    c.carn_date;
 
 
 
